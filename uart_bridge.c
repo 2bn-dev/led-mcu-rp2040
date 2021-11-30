b2b9062fea81b1cb2af0ebe2db0b0c59b134e89d
Matt Mills <mmills@2bn.net>
2021-11-30 12:42:28 -0700

    Update to working version w/ auto flash mode

--- uart_bridge.c
+++ uart_bridge.c
@@ -1,9 +1,9 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/stdio.h"
 #include "hardware/uart.h"
-#include "tusb.h"
 #include "device/usbd_pvt.h"
 
 #define PIN_TXD0 0
@@ -11,11 +11,19 @@
 #define PIN_ESP32_EN 13
 #define PIN_ESP32_FLASH 14
 
+const char ESP_SYNC[] = { 
+	0xc0, 0x00, 0x08, 0x24, 0x00, 0x00, 0x00, 0x00, 
+	0x00, 0x07, 0x07, 0x12, 0x20, 0x55, 0x55, 0x55,
+       	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 
+	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 
+	0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 0x55, 
+	0x55, 0x55, 0x55, 0x55, 0x55, 0xC0
+};
+
 uint32_t current_baud_rate;
 static void uart0_init(uint32_t baud_rate){
 	current_baud_rate = uart_init(uart0, baud_rate);
-	printf("init: %d %d\n" , (uint32_t *)baud_rate, current_baud_rate);
-	//uart_set_format(uart0, 8, 0, 1)
+	uart_set_format(uart0, 8, 1, UART_PARITY_NONE);
 	uart_set_translate_crlf(uart0, false);
 }
 static void uart0_deinit(){
@@ -27,11 +35,12 @@ static void pin_init(void){
         gpio_set_function(PIN_RXD0, GPIO_FUNC_UART);
         gpio_init(PIN_ESP32_FLASH);
         gpio_init(PIN_ESP32_EN);
-        gpio_pull_up(PIN_ESP32_FLASH);
+        gpio_pull_down(PIN_ESP32_FLASH);
 	sleep_ms(1);
 	gpio_pull_up(PIN_ESP32_EN);
 	sleep_ms(1);
 	gpio_pull_down(PIN_ESP32_EN);
+	//This resets ESP32
 }
 
 
@@ -51,65 +60,80 @@ static void pin_init(void){
  * gpio_pull_down(PIN_ESP32_EN);  == chip enable
  */
 
-uint8_t last_rts = 1;
-uint8_t last_dtr = 1;
+
+/* Sequencing from ESPTOOL (seconds):
+ * DTR: 0.0000000 False == TINYUSB 0 ?
+ * RTS: 0.0007728 True == TINYUSB 1
+ * DTR: 0.1010019 True 
+ * RTS: 0.1040874 False
+ * DTR: 0.1542244 False
+ */
+
 
 int main(){
+	stdio_usb_init();
 	uart0_init(115200);
 	pin_init();
-        stdio_usb_init();
-	setvbuf(stdout, NULL, _IONBF, 0);
-	char *outbuf = (char *) malloc(sizeof(char)*1024);
-	//setbuf(stdout, NULL);
+
+	char read_buf[128];
+	int32_t read_buf_write = 0;
+	bool match_found = false;
         while(true){
-		//cdc_line_coding_t* line_coding;
-		//tud_cdc_n_get_line_coding(0, line_coding);
-		//printf("%d %d %d %d\n", line_coding->bit_rate, line_coding->stop_bits, line_coding->parity, line_coding->data_bits);
-		//if(*ptr != current_baud_rate){
-			//uart0_deinit();
-			//uart0_init( *ptr);
-		//}
-		uint8_t line_state = tud_cdc_n_get_line_state(0); //#define USBD_ITF_CDC       (0)
-                uint8_t dtr = line_state & 0b1;
-                uint8_t rts = line_state & 0b10>>1;
-		/*
-                if(dtr == 1 && last_dtr != 1){
-                        gpio_pull_down(PIN_ESP32_FLASH);
-                }else if(dtr == 0 && last_dtr != 0){
-                        gpio_pull_up(PIN_ESP32_FLASH);
-                }
-		last_dtr = dtr;
-		*/
-                if(rts == 1 && last_rts != 1){
-                        gpio_pull_down(PIN_ESP32_EN);
-                }else if(rts == 0 && last_rts != 0 && dtr == 1) {
-                        gpio_pull_up(PIN_ESP32_EN);
-			//uart0_init();
-                }
-		last_rts = rts;
+		while(uart_is_readable(uart0)){
+		        int32_t buf = uart_getc(uart0);
+		        if(buf >= 0){
+				putchar_raw(buf);
+			}
+		}
 		stdio_flush();
-		while(true){
+		uart_tx_wait_blocking(uart0);
+		while(uart_is_writable(uart0)){
 			int32_t buf;
 			buf = getchar_timeout_us(0);
-			if(buf == PICO_ERROR_TIMEOUT){
+			if(buf < 0){
 				break;
+			}
+			uart_putc_raw(uart0, buf);
+
+
+			read_buf[read_buf_write] = buf;
+			read_buf_write++;
+			if(read_buf_write >= sizeof(read_buf)){
+				read_buf_write = 0;
+				
 			}
 
-			uart_putc_raw(uart0, buf);
-		}
-		uint32_t len = 0;
-		while(uart_is_readable(uart0)){
-                        outbuf[len] = uart_getc(uart0);
-			len++;
-			if(len > 1023){
-				break;
+			if(!match_found){
+				for(int32_t i = 0; i < sizeof(read_buf); i++){
+					if(read_buf[i] == 0xC0){
+						int32_t i_orig = i;
+						int32_t ii = 0;
+						for(; ii < sizeof(ESP_SYNC); ii++){
+							if(read_buf[i] != ESP_SYNC[ii]){
+								break;
+							}else{
+								i++;
+								if(i >= sizeof(read_buf)){
+									i = 0;
+								}
+							}
+						}
+						if(ii == sizeof(ESP_SYNC)-1){
+							//ESP Sync magic found, enter flash mode
+							gpio_pull_up(PIN_ESP32_FLASH);
+							gpio_pull_up(PIN_ESP32_EN);
+							sleep_ms(1);
+							gpio_pull_down(PIN_ESP32_EN);
+							match_found = true;
+							break;
+						}else{
+							i = i_orig;
+						}
+					}
+				}
 			}
-                }
-		if( len > 0){
-			outbuf[len] = '\0';
-			fputs(outbuf, stdout);
 		}
-		uart_tx_wait_blocking(uart0);
+		stdio_flush();
 		
 
         }
